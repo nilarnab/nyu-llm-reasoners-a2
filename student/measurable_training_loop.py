12843d2d5edf3df9420a92692d3c4511625e8735
Nilarnab Debnath <nilarnabdebnath@gmail.com>
2026-02-28 15:22:50 -0500

    minor adjustment

--- student/measurable_training_loop.py
+++ student/measurable_training_loop.py
@@ -111,17 +111,16 @@ vocab_size=VOCAB_SIZE,
 
                 conditionally_torch_sync(device)
 
-    if profile_memory:
-        if device == "cuda":
-            torch.cuda.memory._record_memory_history(max_entries=1000000)
-        else:
-            print("PROFILING REQUESTED BUT DEVICE WAS NOT CUDA, skipping")
-
-
     with torch.no_grad():
         for it_id in range(time_measure_params['measure_for_count']):
             with nvtx.range("FULL_EVAL_RUN"):
                 # print("measuring it_id", it_id)
+
+                if profile_memory:
+                    if device == "cuda":
+                        torch.cuda.memory._record_memory_history(max_entries=1000000)
+                    else:
+                        print("PROFILING REQUESTED BUT DEVICE WAS NOT CUDA, skipping")
 
                 if generate_data_randomly:
                     input_tensor, target_tensor = get_random_data(
@@ -140,11 +139,12 @@ vocab_size=VOCAB_SIZE,
 
                 loss = basic_nn_utils.cross_entropy(logits, target_tensor)
 
-    if profile_memory and device == 'cuda':
-        torch.cuda.memory._dump_snapshot(profile_memory_location)
-        torch.cuda.memory._record_memory_history(enabled=None)
+                if profile_memory and device == 'cuda':
+                    torch.cuda.memory._dump_snapshot(profile_memory_location)
+                    torch.cuda.memory._record_memory_history(enabled=None)
 
-        print("PROFILING COMPLETE, EXITING THE LOOP")
+                    print("PROFILING ONE STEP COMPLETE, EXITING THE LOOP")
+                    break
 
 
     return res
